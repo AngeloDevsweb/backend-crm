e0486488af17d714e26d7015f83406a8bf485742
Angelo <gabriel_propio95@hotmail.com>
2024-03-25 17:08:42 -0400

    ruta de register completa, con hash password, token y respuesta de cookie al navegador

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,25 @@
+
+import { createAccessToken } from '../libs/jwt.js'
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
 
 export const register = async(req, res)=>{
     const {email, password, username} = req.body
 
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const newUser = new User({
-            username, email, password
+            username, email, password : passwordHash,
         })
         //codigo para guardar el usuario en la base de datos
         const userSaved = await newUser.save();
+        //utilizamos el token
+        const token = await createAccessToken({id:userSaved._id})
+        //enviamos el id para que lo cree como un token
+        //crea una cookie que ya viene de express, para que se cree directamente la cookie en nuestro navegador
+        res.cookie("token", token)
     
         //respuesta al cliente
         res.json({
