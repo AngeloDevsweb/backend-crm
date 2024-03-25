8ff92e67f5e2bd5485a1bc405c87287383240a98
Angelo <gabriel_propio95@hotmail.com>
2024-03-25 17:58:59 -0400

    añadido la funcion de logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -65,3 +65,10 @@ export const login = async(req, res)=>{
         res.status(400).json({message:error.message})
     }
 }
+
+export const logout = (req, res) =>{
+    res.cookie("token", "", {
+        expires: new Date(0),
+    });
+    return res.sendStatus(200)
+}
