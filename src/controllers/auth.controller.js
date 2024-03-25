c5fce8f9712c9fda6d17d772311e5d1dda31da79
Angelo <gabriel_propio95@hotmail.com>
2024-03-25 17:54:35 -0400

    funcion de login completa + token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,6 +33,35 @@ export const register = async(req, res)=>{
 
 }
 
-export const login = (req, res)=>{
-    res.json("welcome login")
+export const login = async(req, res)=>{
+    const {email, password} = req.body;
+
+    try {
+         //guardamos en la variable el usuario o email que encontramos en la base de datos
+         const userFound = await User.findOne({email})
+         //hacemos una validacion 
+         if(!userFound) return res.status(400).json(["user not found"])
+
+         //logica para verificar el password del usuario con el password de la base de datos del email que encontramos
+         const isMatch = await bcrypt.compare(password, userFound.password)
+         //hacemos una validacion
+         if(!isMatch) return res.status(400).json(["Incorrect password"])
+
+         //utilizar el token
+         const token = await createAccessToken({id: userFound._id}) //enviamos el id para que lo cree como un token
+         //crea una cookie que ya viene de express, para que se cree directamente la cookie en nuestro navegador
+         res.cookie("token", token)
+
+         //respuesta al cliente
+         res.json({
+            email:userFound.email,
+            username: userFound.username,
+            id: userFound.id,
+            createdAt : userFound.createdAt
+         })
+ 
+
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
 }
